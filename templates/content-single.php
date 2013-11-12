863db8ea720ade24ec852d3cc5359ab390a88c7f
Benedicte Raae <raae@bgraphic.no>
2013-11-12 11:59:21 +0100

    Fixes ome styling on entry

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,6 +1,8 @@
 <?php comments_template('/templates/header.php'); ?>
 
+<div class="entry-content">
 <?php the_content(); ?>
+</div>
 
 <footer>
   <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'lkk'), 'after' => '</p></nav>')); ?>
--- templates/header.php
+++ templates/header.php
@@ -2,4 +2,7 @@
   <h1>
     <?php echo lkk_title(); ?>
   </h1>
+  <?php if (is_single()): ?>
+    <?php get_template_part('templates/entry-meta'); ?>
+  <?php endif; ?>
 </header>
