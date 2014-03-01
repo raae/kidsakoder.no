0478fa1e82129b9ac1063534b293be3a4ff87a59
Benedicte Raae <raae@bgraphic.no>
2014-03-01 19:25:52 +0100

    Softer sidebar colors

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0ad33b8d31996a7ba83ec38a1554e6d7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '652e554f2462dba2502cc9bfe06ba88f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
