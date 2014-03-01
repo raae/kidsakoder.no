24b53949d75debc3a30ca2cdb484e2d13dc4055d
Benedicte Raae <raae@bgraphic.no>
2014-03-01 16:18:40 +0100

    Fixed wrong file deletion

---
+++ template-home.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Home Template
+*/
+?>
+
+<?php get_template_part('single-jumbotron'); ?>
+
+<header class="page-header">
+  <h1>
+    <?php _e('Latest Posts', 'roots') ?>
+  </h1>
+</header>
+
+<?php
+
+$posts = query_posts('posts_per_page=6&post_type="post"'); ?>
+
+<?php get_template_part('templates/loop'); ?>
+  
+<?php wp_reset_query(); // reset the query ?>
